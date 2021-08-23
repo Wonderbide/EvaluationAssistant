9125b635b67284ee77fe1474e682cb9787b0749a
unknown <Omar.menssouri@gmail.com>
2021-08-24 00:45:26 +0200

    Project first initialisation

---
+++ EvaluationMaker/src/main/java/com/example/evaluationmaker/EvaluationMakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.evaluationmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvaluationMakerApplication.class, args);
+    }
+
+}
